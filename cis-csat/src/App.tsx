6fd5393aa27ecdba80b60871aaef7ef752ca04b0
EduuIX <eduardomoraisribeiro@hotmail.com>
2024-07-30 07:14:39 -0300

    /front

--- cis-csat/src/App.tsx
+++ cis-csat/src/App.tsx
@@ -5,8 +5,8 @@ import { Header } from './components/header/header';
 import { Dashboard } from './routes/dashboard/dashboard';
 import { CEI01 } from './routes/cei01/cei01';
 import { NotFound } from './routes/notFound/notFound';
-import { CEI02 } from './routes/cei02/cei02';
-import { CEI03 } from './routes/cei03/cei03';
+// import { CEI02 } from './routes/cei02/cei02';
+// import { CEI03 } from './routes/cei03/cei03';
 
 function App() {
     return (
@@ -16,8 +16,8 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Dashboard />} />
                     <Route path="/cei/01" element={<CEI01 />} />
-                    <Route path="/cei/02" element={<CEI02 />} />
-                    <Route path="/cei/03" element={<CEI03 />} />
+                    {/* <Route path="/cei/02" element={<CEI02 />} />
+                    <Route path="/cei/03" element={<CEI03 />} /> */}
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </Router>
